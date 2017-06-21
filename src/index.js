fb8bbe74a54ad9063db367b5df1f50b05ad435c2
Basov Alexandr <bainst4@gmail.com>
2017-06-21 09:45:20 +0300

    + react-scripts deps

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
 
-const API_KEY = 
+const API_KEY = 'AIzaSyBhzlKwUTiKesZur-tz-rBuNJg91oznPwI'
 
 class App extends Component {
   constructor(props) {
